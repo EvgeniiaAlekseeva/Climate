dd4728311fc6b243e5803b9f6299c16a77bd988d
EvgeniiaAlekseeva <47817278+EvgeniiaAlekseeva@users.noreply.github.com>
2019-02-20 16:17:20 +0300

    Add files via upload

--- Phenotypes_cl.m
+++ Phenotypes_cl.m
@@ -1,24 +1,30 @@
 function dX = Phenotypes_cl (t, X, D, b, m, mD, sigma_comp, N, x_cc) 
 % calculates evolutionary dynamics, changing of clusters phenotypes
-% D - number of dimensions/phenotype characteristics 
-% m - number of clusters, m*D - number of equotions
-% x_cc - carrying capacity point with D-coordinates
- 
+% t - time period of ecologicaldynamics
+% X - phenotypic coordinates of clusters, written in a single row (1-D of first cluster, 
+% (D+1)-2D of second and so on);
+% D - dimensionality of the phenotypic space
+% b - mutations coefficients
+% m - number of clusters
+% mD - mxD
+% sigma_comp - width of competition kernel
+% N - population sizes of all clusters
+% x_cc - current location of CCC
+
 dX = zeros(mD,1);
  
-for k = 1:mD                % go through whole vector of equotions
+for k = 1:mD                
     
     if rem(k,D) == 0;
         r = k/D;
         i = D;
     else
-    r = floor (k/D) + 1;    % find index of cluster of this equotion
-    i = rem (k,D);          % find index of phenotype coordinate 
+    r = floor (k/D) + 1;    
+    i = rem (k,D);           
     end
     
-    %zri = D*(r-1)+i;   % i-th phenotype of r-th cluster - the same with k;
-    zr1 = D*(r-1)+1;    % first phenotype of r-th cluster
-    rD = r*D;           % end of r-th cluster phenotype variables
+    zr1 = D*(r-1)+1;    
+    rD = r*D;           
     big_thing = 0;
     
      
@@ -32,7 +38,7 @@ for k = 1:mD                % go through whole vector of equotions
         X(zr1:rD);
                 
         big_thing = big_thing +...
-            competitionD_cl(X(zs1:sD), X(zr1:rD), D, b, sigma_comp, x_cc) * N(end, s).*...         % influence on cluster r
+            competitionD_cl(X(zs1:sD), X(zr1:rD), D, b, sigma_comp, x_cc) * N(end, s).*...         
             ( sum((b(i,:).').* (X((zs1):sD)-(x_cc.')))  - (X(zsi)-X(k))/sigma_comp(i)^2 - ...
             (X(k)-x_cc(i)).^3  );    
     end
